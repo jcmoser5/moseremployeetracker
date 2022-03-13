21c4ea5c5169efd84a15141a3b02c05a68464074
Jonathan Moser <jonathancmoser@gmail.com>
2022-03-13 14:10:06 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,28 +2,27 @@ DROP DATABASE IF EXISTS organization_db;
 CREATE DATABASE organization_db;
 USE organization_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
   id INT AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
   id INT AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(8,2) UNSIGNED NOT NULL,
+  salary DECIMAL(8,2) NOT NULL,
   department_id INT NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
   id INT AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT DEFAULT NULL,
+  manager_id INT DEFAULT null,
   PRIMARY KEY (id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
